285121290883b33c8f0ad32a4e413156088e24a7
amanda1686 <80174591+amanda1686@users.noreply.github.com>
2023-12-21 16:37:47 +0100

    from

--- src/producto.js
+++ src/producto.js
@@ -27,4 +27,17 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 });
 
+// hamuburguesa
+document.addEventListener('DOMContentLoaded', function () {
+  
+  const menuHamburguesa = document.querySelector('.menu_hamburguesa');
+  const menuDesplegable = document.querySelector('.menu_desplegable');
+
+ 
+  menuHamburguesa.addEventListener('click', function () {
+   
+    menuHamburguesa.classList.toggle('active');
+  });
+});
+
   
